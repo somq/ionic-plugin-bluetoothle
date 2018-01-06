e734ff387783f3d7007257b2ff53df1efb692efc
somq <mail.clement@gmail.com>
2018-01-06 19:04:39 +0100

    path fix

--- install.js
+++ install.js
@@ -1,24 +1,16 @@
 /**
- * Nothing is needed here
  * We will just raw copy /dist/@ionic-native/bluetooth-le to node_modules/@ionic-native/bluetooth-le
  */
 
 var ncp = require('ncp').ncp;
-var fs = require('fs');
 
-fs.mkdir('../@ionic-native/bluetooth-le', err => {
+var source = 'node_modules/ionic-plugin-bluetoothle/dist/@ionic-native/bluetooth-le';
+var destination = 'node_modules/@ionic-native/bluetooth-le';
+
+ncp(source, destination, function (err) {
   if (err) {
     return console.error(err);
   }
-  console.log('Created ionic-plugin-bluetooth-le folder successfuly');
+  console.log('Copied ionic-plugin-bluetooth-le files successfuly');
+});
 
-  var source = './dist/@ionic-native/bluetooth-le';
-  var destination = '../@ionic-native/bluetooth-le';
-  
-  ncp(source, destination, function (err) {
-    if (err) {
-      return console.error(err);
-    }
-    console.log('Copied ionic-plugin-bluetooth-le files successfuly');
-   });
-})
