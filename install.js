716dd9b721b5277b1d4b2f7537eba1e033ae185d
Joshua Noel <jnoel@leviton.com>
2018-01-21 11:53:59 -0600

    fixed install paths for postinstall hook

--- install.js
+++ install.js
@@ -4,15 +4,15 @@
 
 var ncp = require('ncp').ncp;
 
-var source = 'node_modules/ionic-plugin-bluetoothle/dist/@ionic-native/bluetooth-le';
-var destination = 'node_modules/@ionic-native/bluetooth-le';
+var source = 'dist/@ionic-native/bluetooth-le';
+var destination = '../@ionic-native/bluetooth-le';
 
 ncp(source, destination, function (err) {
   if (err) {
     return console.error(err);
   }
   console.log('Copied ionic-plugin-bluetooth-le files successfuly');
-  console.log('Source:'+ source);
-  console.log('Destination:'+ destination);
+  console.log('Source: '+ source);
+  console.log('Destination: '+ destination);
 });
 
